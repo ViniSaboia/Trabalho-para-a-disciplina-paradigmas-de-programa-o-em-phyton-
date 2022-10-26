5df096d12230a2510549bdc772f30f41bc971b5f
ViniSaboia <110861585+ViniSaboia@users.noreply.github.com>
2022-10-26 20:18:49 -0300

    Add files via upload

---
+++ ex_peso.py
@@ -0,0 +1,15 @@
+print("/tJoão Papo-de-Pescador/n")
+
+peso = float(input("Peso: "))
+excesso = 0
+multa = 0
+
+if peso <= 50:
+    print("Excesso: %.2f" %excesso)
+    print("Multa: %.2f" %multa)
+else:
+    excesso = peso - 50
+    multa = excesso * 4
+
+    print("Excesso: %.2f" %excesso)
+    print("Multa: %.2f" %multa)
