85de0d7c8f897ed44f11c75d26ffb18fa433f2b5
ViniSaboia <110861585+ViniSaboia@users.noreply.github.com>
2022-10-27 11:09:31 -0300

    Update tinta.py

--- tinta.py
+++ tinta.py
@@ -1,3 +1,6 @@
+# inseri uma variável para tamanho, fiz um cálculo com a metragem dividida pela capacidade de cada lata e caso o resultado dessa divisão seja diferente de zero uma nova lata deve ser adicionada, para me dar a quantidade correta de latas e o valor da compra do total das latas.
+
+
 tamanho = float(input('Quantos metros quadrados devem ser pintados: '))
 
 litros = tamanho / 3.0
